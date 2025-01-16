667bfa40f1ffe6fca95574547cdfbfee9f078a48
George Rabus <george.rabus@isa.utm.md>
2025-01-16 17:52:59 +0200

    blank app

---
+++ pong/pong.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+
+class PongGame(Widget):
+    pass
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+if __name__ == '__main__':
+    PongApp().run()
+    
+
