e89396b890b128933ac82d667de76391c668c0a9
Mohamed dhiaeddine Hassine <hassine-mohamed.dhiaeddine@isa.utm.md>
2025-01-20 18:12:42 +0200

    1B_Added

--- App.py
+++ App.py
@@ -1,9 +1,26 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDButton, MDButtonText, MDButtonIcon
+from kivy.animation import Animation
 
 
-class MainApp(MDApp):
-    def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+
+class MainApp(MDApp):  
+   def build(self):
+        self.theme_cls.primary_palette = "Green"
+        return (
+            MDScreen(
+                MDButton(
+                    MDButtonIcon(
+                        icon="plus",
+                    ),
+                   
+                    style="elevated",
+                    pos_hint={"center_x": 0.9, "center_y": 0.9},
+                ),
+                md_bg_color=self.theme_cls.surfaceColor,
+            )
+        )
+
 
 MainApp().run()
