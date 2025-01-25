fb6504cba0f54fd67d61853587280904e86c3c20
George Rabus <george.rabus@isa.utm.md>
2025-01-25 16:47:28 +0200

    added test cards

--- App.py
+++ App.py
@@ -5,28 +5,34 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty, ListProperty
 from kivymd.uix.chip import MDChip, MDChipText
 from kivymd.uix.list import MDListItem
-from screen_nav import screen_helper
+# from screen_nav import screen_helper
 
 class CustomerScreen(MDScreen):
     pass
+
 class MainScreen(MDScreen):
-    pass
+    def add_order(self):
+        # Builder.load_string(screen_helper)        
+        btn = self.ids.order_btn
+
 
 class FirstPageApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Moccasin"
-        Builder.load_string(screen_helper)
         return MainScreen()
     
-
 class SecondPageApp(MDApp):
     def build(self):
         self.theme_cls.theme_style ="Light"
         self.theme_cls.primary_palette = "Moccasin"
-        Builder.load_string(screen_helper)
+        # Builder.load_string(screen_helper)
         return CustomerScreen()
 
 
 if __name__ == '__main__':
     FirstPageApp().run()
+
+# TODO: 
+# increase font_size
+# fix scrolling behaviour
