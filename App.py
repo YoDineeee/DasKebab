5b463576de0fa7c0176666a00ef30c8efa0912af
George Rabus <george.rabus@isa.utm.md>
2025-01-25 18:04:18 +0200

    button adds orders

--- App.py
+++ App.py
@@ -1,19 +1,41 @@
 from kivymd.app import MDApp
 from kivymd.uix.widget import MDWidget
 from kivymd.uix.screen import MDScreen
-from kivy.lang import Builder
 from kivy.properties import StringProperty, ListProperty
-from kivymd.uix.chip import MDChip, MDChipText
 from kivymd.uix.list import MDListItem
-# from screen_nav import screen_helper
+from kivymd.uix.card import MDCard
+from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDButton, MDIconButton
+from kivymd.uix.relativelayout import RelativeLayout
 
-class CustomerScreen(MDScreen):
-    pass
+class CustomCard(MDCard):
+    def __init__(self, **kwargs):
+        super().__init__(
+            padding="4dp", 
+            size_hint=(1, None),
+            **kwargs
+        )
+        layout = RelativeLayout()
+        icon_btn = MDIconButton(
+            icon="dots-vertical",
+            pos_hint={"top": 1, "right": 1}
+        )
+        label = MDLabel(
+            text="Order Added",
+            adaptive_size=True,
+            text_color="grey",
+            pos_hint={"center_y": 0.5, "center_x": 0.5},
+            bold=True
+        )
+
+        layout.add_widget(icon_btn)        
+        layout.add_widget(label)
+        self.add_widget(layout)
 
 class MainScreen(MDScreen):
     def add_order(self):
-        # Builder.load_string(screen_helper)        
-        btn = self.ids.order_btn
+        stack = self.ids.stack
+        stack.add_widget(CustomCard())
 
 
 class FirstPageApp(MDApp):
@@ -22,16 +44,10 @@ class FirstPageApp(MDApp):
         self.theme_cls.primary_palette = "Moccasin"
         return MainScreen()
     
-class SecondPageApp(MDApp):
-    def build(self):
-        self.theme_cls.theme_style ="Light"
-        self.theme_cls.primary_palette = "Moccasin"
-        # Builder.load_string(screen_helper)
-        return CustomerScreen()
-
 
 if __name__ == '__main__':
     FirstPageApp().run()
+
 
 # TODO: 
 # increase font_size
